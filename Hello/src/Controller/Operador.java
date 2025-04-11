7f10593d2f38e9ec523637b27c40d95c8a5e7991
Alisson <aliba.truta@gmail.com>
2025-04-11 19:33:52 -0300

    concatenacao x adicao

---
+++ Hello/src/Controller/Operador.java
@@ -0,0 +1,13 @@
+package Controller;
+
+public class Operador {
+    public static void main(String[] args) {
+        
+        String concatenacao = "?";
+
+        concatenacao =  "1" + (1 + 5);
+        
+        System.out.println(concatenacao);
+
+    }
+}
