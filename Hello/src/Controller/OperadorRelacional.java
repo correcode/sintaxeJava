a2b063afdd0325df2a31099f2336093c0f6b26ce
Alisson <aliba.truta@gmail.com>
2025-04-12 10:23:10 -0300

    operador relaciona

---
+++ Hello/src/Controller/OperadorRelacional.java
@@ -0,0 +1,16 @@
+package Controller;
+
+public class OperadorRelacional {
+    public static void main(String[] args) {
+        
+        int a,b;
+
+        a = 5;
+        b = 6;
+
+        int resultado = a != b ? 1 : 0;
+
+        System.out.println(resultado);
+
+    }
+}
