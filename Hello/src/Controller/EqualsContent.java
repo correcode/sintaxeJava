82d7f32a458267d144d5de5f37930149a843f825
Alisson <aliba.truta@gmail.com>
2025-04-12 10:35:00 -0300

    equals para comparar objeto

---
+++ Hello/src/Controller/EqualsContent.java
@@ -0,0 +1,11 @@
+package Controller;
+
+public class EqualsContent {
+    public static void main(String[] args) {
+        String nomeUm = "Alisson";
+        String nomeDois = new String ("Alisson"); //novo objeto
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+    }
+}
