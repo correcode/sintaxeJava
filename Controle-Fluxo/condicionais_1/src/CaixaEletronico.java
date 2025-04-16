88ce5d2d979fee8fca7fffc57061bf8d897cab9e
Alisson <aliba.truta@gmail.com>
2025-04-16 19:55:32 -0300

    Sistema de saque condicional

--- Controle-Fluxo/condicionais_1/src/CaixaEletronico.java
+++ Controle-Fluxo/condicionais_1/src/CaixaEletronico.java
@@ -1,11 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class CaixaEletronico {
     public static void main(String[] args) {
-        double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        Scanner scanner = new Scanner (System.in).useLocale(Locale.US);
 
-        if(saldo > valorSolicitado)
-            saldo = saldo - valorSolicitado;
+        int password = 1234;
+        double saldo = 255.19;
 
-        System.out.println(saldo);
+        System.out.println("\nDigite sua senha");
+        int userPassword = scanner.nextInt();
+
+        if(password == userPassword) {
+            System.out.println("\nVoce esta logado, seu saldo e de: " + saldo + "$ Dolares");
+
+            System.out.println("\nQual valor deseja sacar : ");
+            double saque = scanner.nextDouble();
+
+            saldo = saldo - saque;
+
+            System.out.println("Saque efetuado com sucesso!, seu saldo atual e de : " + saldo);
+
+        } else {
+            System.out.println("\nSenha incorreta!");
+        }
     }
 }
