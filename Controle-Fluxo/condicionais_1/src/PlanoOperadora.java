5c3c9c35254c95938ff132fe810c480484fc4603
Alisson <aliba.truta@gmail.com>
2025-04-17 19:50:24 -0300

    modo correto para caso de uso do switch case e nao if else

---
+++ Controle-Fluxo/condicionais_1/src/PlanoOperadora.java
@@ -0,0 +1,17 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "Turbo";
+
+        switch (plano) {
+            case "Turbo": {
+                System.out.println("\n5G Youtube");
+            }
+            case "Midium": {
+                System.out.println("\nWhatsapp Ilimitado");
+            }
+            case "Minimum": {
+                System.out.println("\n10G de Franquia total");
+            }
+        }
+    }
+}
