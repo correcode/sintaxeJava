02d46fdaa2762be645e66f08e9443a940a7770ae
Alisson <aliba.truta@gmail.com>
2025-04-18 09:51:48 -0300

    for exemplo 1

---
+++ Controle-Fluxo/condicionais_1/src/ExemploFor.java
@@ -0,0 +1,7 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int contador = 1; contador <= 20; contador++) {
+            System.out.println("Contagem : " + contador);
+        }
+    }
+}
