7616c79a749e29ab8e6ae6299b87dbcd5b68ce37
Alisson <aliba.truta@gmail.com>
2025-04-18 10:04:42 -0300

    for com arrays e foreach em jav

---
+++ Controle-Fluxo/condicionais_1/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "Alisson", "Caio", "Marcos", "Leonardo" };
+
+            for ( int x=0; x<alunos.length; x++) {
+                System.out.println("O aluno no indice x= " + x + " e " + alunos[x]);
+            }
+    }
+}
---
+++ Controle-Fluxo/condicionais_1/src/ExemploForEach.java
@@ -0,0 +1,9 @@
+public class ExemploForEach {
+    public static void main(String[] args) {
+        String alunos[] = { "Alisson", "Caio", "Marcos", "Leonardo" };
+
+            for (String aluno : alunos) {
+                System.out.println("Nome do aluno e : " + aluno);
+            }
+    }
+}
